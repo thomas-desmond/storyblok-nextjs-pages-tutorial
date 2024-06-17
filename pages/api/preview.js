8e0d84b962c948cbf39255bf3031c0d032a0862b
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-06-17 08:21:00 -0700

    Using main branch

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,30 +1,21 @@
-const IS_PROD = process.env.NODE_ENV === "production";
-
-const setCookieSameSite = (res, value) => {
-  const cookies = res.getHeader("Set-Cookie");
-  res.setHeader(
-    "Set-Cookie",
-    cookies?.map((cookie) =>
-      cookies.replace(
-        cookie,
-        "SameSite=Lax",
-        `SameSite=${value}; ${IS_PROD ? "Secure;" : ""}`
-      )
-    )
-  );
-};
-
-
 export default async function preview(req, res) {
   const { slug = "" } = req.query;
   const params = req.url.split("?");
-
+ 
   if (req.query.secret !== "MY_SECRET_TOKEN") {
     return res.status(401).json({ message: "Invalid token" });
   }
-
+ 
   res.setPreviewData({});
-  setCookieSameSite(res, "None");
-
+ 
+  // Set cookie to None, so it can be read in the Storyblok iframe
+  const cookies = res.getHeader("Set-Cookie");
+  res.setHeader(
+    "Set-Cookie",
+    cookies.map((cookie) =>
+      cookie.replace("SameSite=Lax", "SameSite=None;Secure")
+    )
+  );
+ 
   res.redirect(`/${slug}?${params[1]}`);
 }
