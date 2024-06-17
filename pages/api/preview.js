073542f0f45fbc10f13f361e1b0ff0b0f3b26441
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-06-17 08:15:46 -0700

    Update preview api

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,22 +1,30 @@
-export default async function preview(req, res) {
-    const { slug = "" } = req.query;
-    const params = req.url.split("?");
-   
-    if (req.query.secret !== "MY_SECRET_TOKEN") {
-      return res.status(401).json({ message: "Invalid token" });
-    }
-   
-    res.setPreviewData({});
-   
-    // Set cookie to None, so it can be read in the Storyblok iframe
-    const cookies = res.getHeader("Set-Cookie");
-    res.setHeader(
-      "Set-Cookie",
-      cookies.map((cookie) =>
-        cookie.replace("SameSite=Lax", "SameSite=None;Secure")
+const IS_PROD = process.env.NODE_ENV === "production";
+
+const setCookieSameSite = (res, value) => {
+  const cookies = res.getHeader("Set-Cookie");
+  res.setHeader(
+    "Set-Cookie",
+    cookies?.map((cookie) =>
+      cookies.replace(
+        cookie,
+        "SameSite=Lax",
+        `SameSite=${value}; ${IS_PROD ? "Secure;" : ""}`
       )
-    );
-   
-    res.redirect(`/${slug}?${params[1]}`);
+    )
+  );
+};
+
+
+export default async function preview(req, res) {
+  const { slug = "" } = req.query;
+  const params = req.url.split("?");
+
+  if (req.query.secret !== "MY_SECRET_TOKEN") {
+    return res.status(401).json({ message: "Invalid token" });
   }
 
+  res.setPreviewData({});
+  setCookieSameSite(res, "None");
+
+  res.redirect(`/${slug}?${params[1]}`);
+}
